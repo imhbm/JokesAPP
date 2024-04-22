154387553dea43cb3461766ff11e6e270aea84f6
imhbm <himandribhusan@gmail.com>
2024-04-23 03:48:15 +0530

    log rollback  implimention

---
+++ src/main/java/com/dillsedeveloper/joke/JokeserviceApplication.java
@@ -0,0 +1,15 @@
+package com.dillsedeveloper.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class JokeserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeserviceApplication.class, args);
+    }
+
+}
